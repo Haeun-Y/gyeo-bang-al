758c83cc0854af82d74a42fcfe4aa826981cd59a
Haeun-Y <hahaha329@ewhain.net>
2023-01-13 16:01:59 +0900

    [08] 스택의 활용

---
+++ 윤하은/[08] 스택의 활용/3986.cpp
@@ -0,0 +1,36 @@
+//BOJ 3986 좋은 단어
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string str;
+    int wordNum;
+    cin >> wordNum;
+    int result = 0;
+    
+    for(int i = 0; i<wordNum; i++)
+    {
+        cin >> str;
+        stack<char> letters;
+        
+        for(int i  = 0; i< str.length(); i++)
+        {
+            char ch = str[i];
+            if(!letters.empty() && letters.top() == ch)
+                letters.pop();
+                
+            else
+                letters.push(ch);
+        }
+        
+        if(letters.empty()) result++;
+        
+    }
+    
+    cout << result;
+    
+}
