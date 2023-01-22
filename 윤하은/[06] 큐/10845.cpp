f6daef4f9207c24b770192ca507f9a2909c76847
Haeun-Y <hahaha329@ewhain.net>
2023-01-22 22:41:46 +0900

    [05] 스택 - 문제

---
+++ 윤하은/[06] 큐/10845.cpp
@@ -0,0 +1,62 @@
+//BOJ 10845 큐
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    queue<int> q;
+    int commandNum;
+    cin >> commandNum;
+
+    for(int i = 0; i<commandNum; i++)
+    {
+        string command;
+        cin >> command;
+
+        if(command == "push")
+        {
+            int num;
+            cin >> num;
+            q.push(num);
+
+        }
+        else if(command == "pop")
+        {
+            if(q.empty()) cout << -1;
+            else 
+            {
+                cout << q.front();
+                q.pop();
+            }
+            cout << "\n";
+
+        }
+        else if(command == "size")
+        {
+            cout << q.size() << "\n";
+        }
+        else if(command == "empty")
+        {
+            cout << q.empty() << "\n";
+        }
+        else if(command == "front")
+        {
+            if(q.empty()) cout << -1;
+            else
+                cout << q.front();
+            cout << "\n";
+
+        }
+        else //command == "back"
+        {
+            if(q.empty()) cout << -1;
+            else
+                cout << q.back();
+            cout << "\n";
+
+        }
+    }
+}
