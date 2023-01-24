693c01afbfdbcc99f667cdaad0a78e5679b0ae4b
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2023-01-24 09:47:27 +0900

    [08] 스택의 활용 - 응용 문제

--- 윤하은/[08] 스택의 활용/10799.cpp
+++ 윤하은/[08] 스택의 활용/10799.cpp
@@ -39,5 +39,61 @@ int main(void)
             sticks.push(str[i]);
         //printStack(sticks);
     }
+    
     cout << result;
 }
+
+//다른 풀이
+/*
+//BOJ 10799 쇠막대기
+#include <bits/stdc++.h>
+using namespace std;
+bool isLaser(string& str, int i)
+{
+    //i가 마지막 원소의 인덱스일 경우
+    if(i == str.length()-1) return false;
+    
+    else if(str[i] == '(' && str[i+1] == ')') return true;
+    else return false;
+}
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string str;
+    cin >> str;
+    
+    stack<pair<char, int>> sticks;
+    
+    int laserNum = 0;
+    int result = 0;
+    
+    for(int i = 0; i<str.length(); i++)
+    {
+        if(isLaser(str, i))
+        {
+            i++;
+            laserNum++;
+        }
+        else
+        {
+            if(str[i] == '(')
+                sticks.push({str[i], laserNum});
+            else//str[i] == ')'
+            {
+                    result += (laserNum - sticks.top().second + 1);
+                    sticks.pop();
+                    
+            }
+        }
+        
+        
+        
+    }
+
+    cout << result;
+}
+
+
+*/
