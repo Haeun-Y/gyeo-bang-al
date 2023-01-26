5a7c267fdb5aeba1566d85c1c627c18faee37dbf
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2023-01-26 23:52:59 +0900

    [11] 재귀 - 연습 문제

--- 윤하은/[11] 재귀/1629.cpp
+++ 윤하은/[11] 재귀/1629.cpp
@@ -31,3 +31,35 @@ int main() {
 
     
 }
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int func1(int a, int b, int c)
+{
+  if(b==1) return a%c;
+  else if(b%2 == 1) return ((a%c)*func1(a-1, b, c))%c;
+  else
+  {
+    int num= func1(a, b/2, c);
+    return (num * num)%c;
+  }
+  
+}/*
+long long func2(long long a, long long b, long long c)
+{//a^b= (a^2)^b/2
+  if(b == 0) return a%c;
+  else if(b%2==1) return a;
+  else return func2(a, b,c);
+  
+}*/
+int main(void)
+{
+  //ios::sync_with_stdio(false);
+  //cin.tie(NULL);
+
+  //a^b mod c
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << func1(a, b, c);
+}
+*/
