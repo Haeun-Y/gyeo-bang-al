3eff01e76c167a7398dbd56121c8bd9c1ee2960e
Haeun-Y <hahaha329@ewhain.net>
2024-01-25 22:35:34 +0900

    [16] 다이나믹 프로그래밍 - 기본 문제

--- 윤하은/[16] 다이나믹 프로그래밍/11055.cpp
+++ 윤하은/[16] 다이나믹 프로그래밍/11055.cpp
@@ -1,45 +1,34 @@
 //240123
 //BOJ 11055 가장 큰 증가하는 부분 수열
 //SILVER 2
-//시간 초과
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int n, result;
+vector<int> seqSum;
 vector<int> seq;
-void func1(int idx, int beforeElement, int sum)
-{
-    if(idx == n)
-    {
-        if(sum > result)
-            result = sum;
-        return;
-    }
-    else
-    {
-        if(seq[idx] >= beforeElement)
-        {
-            func1(idx+1, seq[idx], sum + seq[idx]);
-            func1(idx+1, beforeElement, sum);
-        }
-        else
-            func1(idx+1, beforeElement, sum);
-        
-    }
-
-}
 int main(void)
 {
     //ios::sync_with_stdio(false);
     //cin.tie(NULL);
 
     cin >> n;
+
+    seqSum = vector<int>(n, 0);
     seq = vector<int>(n, 0);
+
     for(int i = 0; i<n; i++)
+    {
         cin >> seq[i];
 
-    func1(0, 0, 0);
+        for(int j = i-1; j >= 0; j--)
+        {
+            if(seq[i] > seq[j] && seqSum[j] > seqSum[i])
+                seqSum[i] = seqSum[j];
+        }
+        seqSum[i] += seq[i];
+    }
 
-    cout << result;
-    
+    cout << *max_element(seqSum.begin(), seqSum.end());
 }
