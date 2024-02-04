1331d99fc1806646605bd95ceed908a1e16f4198
Haeun-Y <hahaha329@ewhain.net>
2024-02-04 21:07:36 +0900

    [13] 시뮬레이션 - 기본 문제

--- 윤하은/[13] 시뮬레이션/14503.cpp
+++ 윤하은/[13] 시뮬레이션/14503.cpp
@@ -1,138 +1,81 @@
-//BOJ 14503 로봇 청소기
-#include <bits/stdc++.h>
+//240204
+//BOJ 14503 로봇청소기
+//GOLD 5
+#include <iostream>
+#include <vector>
 using namespace std;
-vector<vector<int>> v;
-vector<vector<bool>> isCleaned;
-int dx[4] = {0, -1, 0, 1};
-int dy[4] = {-1, 0, 1, 0};
-bool isCleanedOrWall(pair<int, int> pos)
+typedef struct
 {
-  for(int i = 0; i<4; i++)
-    {
-      int nx = pos.first + dx[i];
-      int ny = pos.second + dy[i];
+  int x;
+  int y;
+  int d;//direction
+}robot;
+robot cleaner;
+const int NOT_CLEANED = 0;
+const int WALL = 1;
+const int CLEANED = 2;
+int row, col;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+vector<vector<int>> room;
+int cleanRoom()
+{
+  int area = 0;
+  bool isActivated = true;
 
-      if(v[nx][ny] != 1 && !isCleaned[nx][ny]) return false;
+  while(isActivated)
+  {
+    if(room[cleaner.x][cleaner.y] == NOT_CLEANED)
+    {
+      room[cleaner.x][cleaner.y] = CLEANED;
+      area++;
     }
-  return true;
-}
-int clean(pair<int, int> robot, int direction)
-{
-  int result = 0;
-  pair<int, int> position = robot;
-  int curD = direction;
-  
-  while(1)
+    bool isNotcleanedSpaceExist = false;
+    for(int i = 0; i<4; i++)
     {
-      int check = 0;
-      int x = position.first;
-      int y = position.second;
-      //현재 위치를 청소한다. 
-      if(!isCleaned[x][y])
+      cleaner.d = (cleaner.d+3)%4;
+      int nx = cleaner.x + dx[cleaner.d];
+      int ny = cleaner.y + dy[cleaner.d];
+      if(nx < 0 || nx >= row || ny < 0 || ny >= col) continue;
+      if(room[nx][ny] == NOT_CLEANED)
       {
-        isCleaned[x][y] = true;
-        result++;
-      }
-
-      while(1)
-      {
-        int leftSide = curD-1;
-        if(leftSide == -1) leftSide = 3;
-
-        if(!isCleaned[x+dx[curD]][y+dy[curD]])
-        {
-          position.first += dx[curD];
-          position.second += dy[curD];
-          curD = leftSide;
-          isCleaned[position.first][position.second] = true;
-          result++;
-          check = 1;
-          break;
-        }
-        else if(isCleanedOrWall(position))
-        {
-          pair<int, int> back = position;
-          
-          switch(curD)
-            {
-              //북쪽
-              case 0:
-                back.first += 1;
-                break;
-              //동쪽
-              case 1:
-                back.second -= 1;
-                break;
-              //남쪽
-              case 2:
-                back.first -= 1;
-                break;
-              //서쪽
-              case 3:
-                back.second += 1;
-                break;
-            }
-          if(v[back.first][back.second] == 1)
-          {
-            check = -1;
-            break;
-          }
-
-          position = back;
-          continue;
-          
-        }
-        else
-        {
-          curD = leftSide;
-          continue;
-        }
-
-        
-
-      }
-      
-
-      if(check==1)
-        continue;
-      else if(check == -1)
+        cleaner.x = nx;
+        cleaner.y = ny;
+        isNotcleanedSpaceExist = true;
         break;
-      
+      }
     }
-  return result;
-  
+    if(!isNotcleanedSpaceExist)
+    {
+      int nx = cleaner.x - dx[cleaner.d];
+      int ny = cleaner.y - dy[cleaner.d];
+      if(nx < 0 || nx >= row || ny < 0 || ny >= col || room[nx][ny] == WALL)
+        isActivated = false;
+      else
+      {
+        cleaner.x = nx;
+        cleaner.y = ny;
+      }
+    }
+
+      
+  }
+  return area;
 }
 int main(void)
 {
-  //ios::sync_with_stdio(false);
-  //cin.tie(NULL);
-
-  int row, col;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
   cin >> row >> col;
-  vector<int> tmp(col);
-  vector<bool> tmp2(col);
+  room = vector<vector<int>>(row, vector<int>(col, 0));
+  cin >> cleaner.x >> cleaner.y >> cleaner.d;
 
   for(int i = 0; i<row; i++)
-    {
-      v.push_back(tmp);
-      isCleaned.push_back(tmp2);
-    }
+  {
+    for(int j = 0; j<col; j++)
+      cin >> room[i][j];
+  }
 
-  int direction;
-  pair<int, int> robot;
-
-  cin >> robot.first >> robot.second;
-  cin >> direction;
-
-  for(int i = 0; i<row; i++)
-    {
-      for(int j = 0; j<col; j++)
-        cin >> v[row][col];
-    }
-
-  cout << clean(robot, direction);
-  
-  
-
-  
+  cout << cleanRoom();
 }
