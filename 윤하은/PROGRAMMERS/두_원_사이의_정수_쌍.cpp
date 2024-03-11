eab9cc6982f7f369b9b110b3646c532e36a4be12
Haeun-Y <hahaha329@ewhain.net>
2024-03-11 23:35:26 +0900

    [PROGRAMMERS] 두 원 사이의 정수 쌍

---
+++ 윤하은/PROGRAMMERS/두_원_사이의_정수_쌍.cpp
@@ -0,0 +1,25 @@
+//240311
+//PROGRAMMERS 두 원 사이의 정수 쌍
+//Lv.2
+//60
+#include <bits/stdc++.h>
+using namespace std;
+bool isIncluded(int x, int y, int r1, int r2)
+{
+    double dist = sqrt(x*x + y*y);
+    if(dist >= r1 && dist <= r2)
+        return true;
+    else return false;
+}
+long long solution(int r1, int r2) {
+    long long answer = 0;
+    for(int x = -r2; x<= r2; x++)
+    {
+        for(int y = -r2; y<= r2; y++)
+        {
+            if(isIncluded(x, y, r1, r2))
+                answer++;
+        }
+    }
+    return answer;
+}
